3c75b5cba06e0384e776609d41af79d670037a0e
PRIEST-PC\Priest <priest_kod@mail.ru>
2017-08-26 16:42:46 +0900

    BT24 Tank crosshair change color

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -6,7 +6,17 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
-//Forward Declaration
+// Enum for aiming state
+UENUM()
+enum class EFiringState : uint8
+{
+	Reloading,
+	Aiming,
+	Locked
+};
+
+
+// Forward Declaration
 class UTankBarrel; 
 class UTankTurret;
 
@@ -23,14 +33,16 @@ public:
 	UTankAimingComponent();
 
 	void SetBarrelReference(UTankBarrel* BarrelToSet);
-
 	void SetTurretReference(UTankTurret* TurretToSet);
 
 	void AimAt(FVector HitLocation, float LaunchSpeed);
 
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	EFiringState FiringState = EFiringState::Aiming;
+
 private:
 	UTankBarrel* Barrel = nullptr;
-
 	UTankTurret* Turret = nullptr;
 
 	void MoveBarrelTowards(FVector AimDirection);
